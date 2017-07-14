6af0797a8f2bdbbd8cd555c4a0677495b9d7b6c1
synion <lach.szymon86@gmail.com>
2017-07-14 19:41:10 +0200

    Add genre spec

--- Gemfile
+++ Gemfile
@@ -27,5 +27,9 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :test do
+  gem 'shoulda-matchers', require: false
+end
+
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ spec/models/genre_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Genre, type: :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_uniqueness_of :name}
+  end
+  describe 'database columns' do
+    it { should have_db_column :name }
+  end
+  describe 'associations' do
+    it { is_expected.to have_many :movies }
+  end
+end
