eb6fd63b5a363b26a448cd429fa3a445fbf4d89c
synion <lach.szymon86@gmail.com>
2017-07-14 19:32:12 +0200

    Add movie spec

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Movie, type: :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :revenue }
+  end
+  describe 'database columns' do
+    it { should have_db_column :name }
+    it { should have_db_column :revenue }
+  end
+  describe 'associations' do
+    it { is_expected.to have_one :genre }
+  end
+end
